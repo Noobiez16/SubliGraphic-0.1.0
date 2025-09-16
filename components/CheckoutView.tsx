00086248f47eef37b1ec093430457e187e6b8124
Noobiez_ez <152230489+Noobiez16@users.noreply.github.com>
2025-09-15 22:00:03 -0400

    feat: Update version and add PayPal payment option
    Updates the application version to 0.2.0 and introduces a PayPal payment option to the checkout view. The ATH Móvil and Apple Pay buttons have also been slightly refactored for clarity.

--- components/CheckoutView.tsx
+++ components/CheckoutView.tsx
@@ -71,21 +71,29 @@ const CheckoutView: React.FC<CheckoutViewProps> = ({ cart, theme, onBackToCart, 
 
                 {/* Payment Methods */}
                 <div className="space-y-3">
+                    {/* PayPal Button */}
                     <button onClick={handlePayment} disabled={isProcessing} className={`${paymentButtonBase} ${isIOS ? paymentButtonIOS : paymentButtonAndroid}`}>
                         <img src="https://www.paypalobjects.com/webstatic/mktg/Logo/pp-logo-200px.png" alt="PayPal" className="h-6" />
                     </button>
 
-                    <button onClick={handlePayment} disabled={isProcessing} className={`${paymentButtonBase} ${isIOS ? paymentButtonIOS : paymentButtonAndroid} bg-[#E87324] hover:bg-[#D4651A]`}>
-                        <img src="https://i.imgur.com/u8KES31.png" alt="ATH Móvil" className="h-8" />
+                    {/* ATH Mobile Button */}
+                    <button onClick={handlePayment} disabled={isProcessing} className={`${paymentButtonBase} bg-[#E87324] hover:bg-[#D4651A]`}>
+                        <img src="https://i.imgur.com/u8KES31.png" alt="ATH Mobile" className="h-8" />
                     </button>
 
-                    <div className="grid grid-cols-2 gap-3">
-                        <button onClick={handlePayment} disabled={isProcessing} className={`${paymentButtonBase} ${isIOS ? 'bg-black text-white hover:bg-gray-800' : paymentButtonAndroid}`}>
-                             <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_Pay_logo.svg" alt="Apple Pay" className={`h-6 ${isIOS ? 'filter invert' : ''}`} />
-                        </button>
-                        <button onClick={handlePayment} disabled={isProcessing} className={`${paymentButtonBase} ${isIOS ? paymentButtonIOS : paymentButtonAndroid}`}>
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/b/b5/Google_Pay_mark_800.svg" alt="Google Pay" className="h-7"/>
-                        </button>
+                    {/* Apple Pay & Google Pay Buttons */}
+                    <div className="grid grid-cols-1 gap-3">
+                        {isIOS ? (
+                            // Apple Pay button (Only shown on iOS)
+                            <button onClick={handlePayment} disabled={isProcessing} className={`${paymentButtonBase} bg-black text-white hover:bg-gray-800`}>
+                                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-7 fill-current"><title>Apple Pay</title><path d="M14.885 8.65a2.532 2.532 0 00-1.546.685 2.215 2.215 0 00-.67 1.488.015.015 0 00.015.015h.004a1.96 1.96 0 011.61-1.123 1.93 1.93 0 011.399.535c-.473.282-.9.62-1.213 1.025a4.293 4.293 0 00-1.261 2.494c.057.004.113.004.169.004.59 0 1.152-.165 1.621-.448a2.55 2.55 0 001.08-1.303c.03-.09.053-.185.071-.282a1.88 1.88 0 00-1.282-2.108zm-5.06-2.52a4.42 4.42 0 012.215-.603c.338.004.678.04 1.01.104a4.34 4.34 0 00-2.333.91 4.444 4.444 0 00-1.748 2.253c-.3.832-.44 1.703-.424 2.578a10.87 10.87 0 00.51 3.251 3.22 3.22 0 002.822 2.148 3.1 3.1 0 003.04-1.22c.237-.36.434-.75.586-1.157a.016.016 0 00-.02-.018l-.008-.002a3.31 3.31 0 01-2.9-2.012 3.42 3.42 0 01-.13-1.603c0-.05.003-.1.003-.148a4.426 4.426 0 011.45-3.09 4.38 4.38 0 013.01-1.393 4.47 4.47 0 012.44 1.156 5.92 5.92 0 00-2.28 1.487 5.9 5.9 0 00-1.892 3.518c-.015.086-.027.172-.043.258a.015.015 0 00.015.017h.002l.02-.001a4.22 4.22 0 003.95-3.13 4.3 4.3 0 00-2.2-4.96c-1.353-.82-3.03-.9-4.53-.41a5.9 5.9 0 00-4.248 2.37zM12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0z"/></svg>
+                            </button>
+                        ) : (
+                            // Google Pay button (Only shown on Android and others)
+                            <button onClick={handlePayment} disabled={isProcessing} className={`${paymentButtonBase} ${paymentButtonAndroid}`}>
+                                <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-7 fill-current"><title>Google Pay</title><path d="M20.895 9.425a2.208 2.208 0 00-2.2-2.2h-9.39a2.21 2.21 0 00-2.2 2.2v5.15a2.21 2.21 0 002.2 2.2h9.39a2.21 2.21 0 002.2-2.2V9.425zm-6.233 4.135h-1.932v2.18h-1.39v-2.18h-1.57v-1.18h1.57v-1.63c0-.988.46-2.02 2.015-2.02.58 0 .97.05 1.22.08v1.17h-.72c-.44 0-.58.2-.58.55v.72h1.31l-.165 1.18z"/><path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 21.818C6.58 21.818 2.18 17.42 2.18 12S6.58 2.182 12 2.182s9.818 4.398 9.818 9.818-4.398 9.818-9.818 9.818z"/></svg>
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
